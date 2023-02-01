8c045110f1fb2c2777e75f70c0d32d5a362aad02
Ben-Langdon <benjaminlangdon@googlemail.com>
2023-02-01 20:38:01 +0000

    Removed pause timer function and replaced with logged time total

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 
 const btn = document.querySelector('.start-timer')
 const display = document.querySelector('.display')
+const logSection = document.querySelector('.previous-logs')
 
 
 btn.addEventListener('click', startTimer)
 
 
 let startTime;
-let loggedTime;
-let updateInterval
+let timeSinceStart;
+let updateInterval;
+let loggedTimeTotal = 0;
 
 function startTimer() {
     startTime = new Date()
     updateInterval = setInterval(update, 1000)
-    btn.innerText = 'Pause'
+    btn.innerText = 'Stop / Log'
     btn.removeEventListener('click', startTimer)
-    btn.addEventListener('click', pauseTimer)
+    btn.addEventListener('click', stopTimer)
 }
 
 
 function update() {
-    loggedTime = (new Date() - startTime) / 3600000
-    display.innerText = `hours: ${loggedTime.toString().slice(0,6)}`
+    timeSinceStart = (new Date() - startTime) / 3600000
+    display.innerText = `Hours: ${timeSinceStart.toString().slice(0,6)}`
 }
 
-function pauseTimer() {
-    btn.innerText = 'Resume'
+function stopTimer() {
+    btn.innerText = 'Start Timer'
     clearInterval(updateInterval)
-    btn.removeEventListener('click', pauseTimer)
-    btn.addEventListener('click', resumeTimer)
-}
-
-function resumeTimer() {
-    alert('working')
+    btn.removeEventListener('click', stopTimer)
+    btn.addEventListener('click', startTimer)
+    loggedTimeTotal += timeSinceStart
+    logSection.innerText = loggedTimeTotal
 }
