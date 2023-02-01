14612eafdbd50710792bcff7a1da61578eb9740d
Ben-Langdon <benjaminlangdon@googlemail.com>
2023-02-01 19:22:26 +0000

    added timer in seconds

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 
 const btn = document.querySelector('.start-timer')
+const display = document.querySelector('.display')
 
 
 btn.addEventListener('click', startTimer)
 
 
+let timeNow;
+let loggedTime;
+
 function startTimer() {
+    timeNow = new Date()
+    setInterval(update, 1000)
     btn.innerText = 'Stop Timer'
     btn.removeEventListener('click', startTimer)
     btn.addEventListener('click', stopTimer)
 }
 
+
+function update() {
+    loggedTime = (new Date() - timeNow) / 1000
+    display.innerText = `${loggedTime} Seconds`
+}
 
 function stopTimer() {
     btn.innerText = 'Start Timer'
